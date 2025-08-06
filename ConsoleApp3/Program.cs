585f2a508b327dd65cbb9be54c3eb21c0c3b0a77
Matthew Hamilton <mwhamilton6@gmail.com>
2025-08-06 10:48:30 -0500

    All classes are filled out to the best of my ability, but still no progress :(

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,12 +9,19 @@ using Microsoft.PowerBI.Client.Windows.Services;
 using Microsoft.PowerBI.Client.Windows.Telemetry;
 using Microsoft.PowerBI.Client.Windows;
 using Microsoft.PowerBI.Client.Shared;
+using Microsoft.Mashup.Host.Document;
+using Microsoft.PowerBI.Client.Windows.Utilities;
+using Microsoft.PowerBI.Client.Telemetry;
+using Microsoft.ReportingServices.Library;
+using System.Threading;
+using System.Collections.Generic;
+using Microsoft.PowerBI.DataExtension.Contracts.Internal;
 
 namespace ConsoleApp3
 {
     class Program
     {
-        static (EngineDataModel, PowerViewHandler) GetEngineDataModel(string dbname, int port)
+        static (EngineDataModel, PowerViewHandler) GetEngineDataModel(FeatureSwitches featureSwitches, string dbname, int port)
         {
             string connStr = $"Provider=MSOLAP.8;Persist Security Info=True;Initial Catalog={dbname};Data Source=localhost:{port};MDX Compatibility=1;Safety Options=2;MDX Missing Member Mode=Error;Update Isolation Level=2;";
             //var test = new Connection(connStr);
@@ -22,7 +29,7 @@ namespace ConsoleApp3
             //var reader = test.CreateCommand("Evaluate example").ExecuteReader();
             //Console.WriteLine(reader.NextResult());
             //return;
-            var powerViewHandler = new PowerViewHandler();
+            var powerViewHandler = new PowerViewHandler(featureSwitchProxy: featureSwitches.FeatureSwitchesProxy);
             var connInfo = ASConnectionInfo.CreateLocalConnectionInfo(
                databaseName: dbname,
                databaseID: dbname,
@@ -32,7 +39,7 @@ namespace ConsoleApp3
             var engineDataModel = powerViewHandler.GetEngineDataModel(
                 connInfo.DatabaseID,
                 "2.0",
-                null,
+                TranslationsBehavior.Default,
                 ExploreHostUtils.GetEngineDataModel
             );
             return (engineDataModel, powerViewHandler);
@@ -40,43 +47,59 @@ namespace ConsoleApp3
         static FeatureSwitches InitializeFeatureSwitches()
         {
             var systemEnvironment = new SystemEnvironment();
-            //var powerBISettings = PowerBISettingsFactory.GetSettingsFromAppConfig(systemEnvironment);
+            var powerBISettings = PowerBISettingsFactory.GetSettingsFromAppConfig(systemEnvironment);
+            var constants = new PowerBIConstants(
+                powerBISettings,
+                systemEnvironment
+            );
+            DependencyInjectionService.Get().RegisterInstance((IApplicationPaths)constants);
+            DependencyInjectionService.Get().RegisterInstance((IApplicationSettings)powerBISettings);
+
             var manager = new FeatureSwitchManager(
                 new VersionInfo(),
-                
-                null, //new PowerBIConstants(
-                //    null, // powerBISettings, 
-                //    systemEnvironment
-                //),
-                null,
-                null,
-                null
+                constants,
+                new PowerBIMockTelemetryService(),
+                powerBISettings,
+                new WebView2VersionUtils()
             );
-            // manager.RegisterKnownSwitches();
+            manager.RegisterKnownSwitches();
             var proxyService = new FeatureSwitchProxyService(manager);
             return new FeatureSwitches(proxyService);
         }
-        static ExploreClientHandlerContext GetContext(PowerViewHandler powerViewer)
+        static ExploreClientHandlerContext GetContext(PowerViewHandler powerViewer, FeatureSwitches featureSwitches)
         {
             return new ExploreClientHandlerContext(
                 powerViewer,
                 DataShapeEngine.Instance, // seems good
-                InitializeFeatureSwitches(),
+                featureSwitches,
                 new QueryCancellationManager()  // seems good imo
             );
+        }
+        static PowerBIMockTelemetryService getTelemetry()
+        {
+            var telemetry = new PowerBIMockTelemetryService();
+            DependencyInjectionService.Get().RegisterInstance((IPowerBITelemetryService)telemetry);
+            return telemetry;
         }
         static void Main(string[] args)
         {
             var dbName = "628cbc55-9fee-4bff-95dd-6b09256e3ba0";
             var port = 63751;
-            var (engineDataModel, powerViewer) = GetEngineDataModel(dbName, port);
-            var context = GetContext(powerViewer);
+            var telemetry = getTelemetry();
+            var featureSwitches = InitializeFeatureSwitches();
+            var (engineDataModel, powerViewer) = GetEngineDataModel(featureSwitches, dbName, port);
+            var context = GetContext(powerViewer, featureSwitches);
             var queryFlow = new TranslateDataViewQueryFlow(
                 context: context,
                 databaseID: dbName,
                 definition: Query.GetQuery()
             );
             queryFlow.Translate(engineDataModel);
+            foreach (KeyValuePair<string, string> kvp in queryFlow.Result.SelectNameToDaxColumnName)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            }
+
             var expr = queryFlow.Result.DaxExpression;
             Console.WriteLine(expr != null);
             //Console.WriteLine($"Length: {expr.Length}, Query: {expr}");
