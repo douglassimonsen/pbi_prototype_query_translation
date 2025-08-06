1ccc8e6867731fea2b43557f2a4acc0fa6154559
Matthew Hamilton <mwhamilton6@gmail.com>
2025-08-06 10:48:29 -0500

    Fully processed data, but I think I'm missing the right data model?

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,13 +2,15 @@
 using Microsoft.PowerBI.ExploreHost.Utils;
 using Microsoft.DataShaping.Engine;
 using Microsoft.PowerBI.ExploreHost.SemanticQuery;
+using Microsoft.PowerBI.ExploreHost;
 using System;
+using Microsoft.BusinessIntelligence;
 
 namespace ConsoleApp3
 {
     class Program
     {
-        static EngineDataModel GetEngineDataModel(string dbname, int port)
+        static (EngineDataModel, PowerViewHandler) GetEngineDataModel(string dbname, int port)
         {
             string connStr = $"Provider=MSOLAP.8;Persist Security Info=True;Initial Catalog={dbname};Data Source=localhost:{port};MDX Compatibility=1;Safety Options=2;MDX Missing Member Mode=Error;Update Isolation Level=2;";
             //var test = new Connection(connStr);
@@ -29,20 +31,32 @@ namespace ConsoleApp3
                 null,
                 ExploreHostUtils.GetEngineDataModel
             );
-            return engineDataModel;
+            return (engineDataModel, powerViewHandler);
+        }
+        static ExploreClientHandlerContext GetContext(PowerViewHandler powerViewer)
+        {
+            return new ExploreClientHandlerContext(
+                powerViewer,
+                new DataShapeEngine(),
+                new FeatureSwitches(),
+                new QueryCancellationManager()
+            );
         }
         static void Main(string[] args)
         {
             var dbName = "2e7de914-94c5-4257-bc86-4b07668bb9c5";
             var port = 56595;
-            var engineDataModel = GetEngineDataModel(dbName, port);
+            var (engineDataModel, powerViewer) = GetEngineDataModel(dbName, port);
+            var context = GetContext(powerViewer);
             var queryFlow = new TranslateDataViewQueryFlow(
-                context: null,
+                context: context,
                 databaseID: dbName,
                 definition: Query.GetQuery()
             );
             queryFlow.Translate(engineDataModel);
-            Console.WriteLine(queryFlow.Result);
+            var expr = queryFlow.Result.DaxExpression;
+            Console.WriteLine(expr != null);
+            //Console.WriteLine($"Length: {expr.Length}, Query: {expr}");
         }
     }
 }
