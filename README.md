12d94b1cc3f36e8b4dc60513e4bde07ce998c164
Matthew Hamilton <mwhamilton6@gmail.com>
2025-08-06 10:48:39 -0500

    beginning cleanup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Context
+
+PowerBI reports generally include visuals such as bar charts, pie charts, etc. Since these visuals should be agnostic of the specific SSAS backend they pull data from, they internally contain an object called the "Prototype Query". When the visual needs to pull data, this Prototype Query is passed alongside 
+
+
+## Prototype Query Design
+
--- python_bindings/pbi_translation/main.py
+++ python_bindings/pbi_translation/main.py
@@ -1,27 +1,28 @@
 import sys
-import clr  # type: ignore[import-untyped]
 from pathlib import Path
-SOURCE_FOLDER = (Path(__file__).parent / 'libs').absolute().as_posix()
+from typing import TYPE_CHECKING
+
+import clr  # type: ignore[import-untyped]
+
+SOURCE_FOLDER = (Path(__file__).parent / "libs").absolute().as_posix()
 sys.path.insert(0, SOURCE_FOLDER)
 clr.AddReference("Translation")  # type: ignore
-from Translation import PrototypeQuery, DataViewQueryTranslationResult
-
-
-
-def prototype_query(query: str, db_name: str, port: int) -> "DataViewQueryTranslationResult":
-    return PrototypeQuery.Translate(
-        query,
-        db_name,
-        port,
-        SOURCE_FOLDER
+if TYPE_CHECKING:
+    from .Translation import (  # type: ignore[import-untyped]
+        DataViewQueryTranslationResult,
+        PrototypeQuery,
     )
+else:
+    from Translation import DataViewQueryTranslationResult, PrototypeQuery
 
 
-if __name__ == '__main__':
+def prototype_query(
+    query: str, db_name: str, port: int
+) -> "DataViewQueryTranslationResult":
+    return PrototypeQuery.Translate(query, db_name, port, SOURCE_FOLDER)
+
+
+if __name__ == "__main__":
     query = open(r"C:\Users\USER\Documents\repos\prototype_parsing\data.json").read()
-    x = prototype_query(
-        query,
-        "e1ff5407-9b29-4692-870b-41bacbb9c4f5",
-        50025
-    )
+    x = prototype_query(query, "e1ff5407-9b29-4692-870b-41bacbb9c4f5", 50025)
     print(x.DaxExpression)
