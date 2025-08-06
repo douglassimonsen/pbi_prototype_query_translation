283e131b04217dac094c209b2fb77f59e91277c6
Matthew Hamilton <mwhamilton6@gmail.com>
2025-08-06 10:48:40 -0500

    Added comment

--- python_bindings/pbi_translation/main.py
+++ python_bindings/pbi_translation/main.py
@@ -7,6 +7,7 @@ import clr  # type: ignore[import-untyped]
 SOURCE_FOLDER = (Path(__file__).parent / "libs").absolute().as_posix()
 sys.path.insert(0, SOURCE_FOLDER)
 clr.AddReference("Translation")  # type: ignore
+# When type checking, we import from the stub file. When running, we import from the actual C# module.
 if TYPE_CHECKING:
     from .Translation import (  # type: ignore[import-untyped]
         DataViewQueryTranslationResult,
