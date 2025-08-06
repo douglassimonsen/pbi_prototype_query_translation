7f073f27f15b9438b7268eff85bdd003efd93aa9
Matthew Hamilton <mwhamilton6@gmail.com>
2025-08-06 10:48:39 -0500

    Added dynamic versions. Fixed types on the result C# object

--- python_bindings/pbi_translation/Translation.pyi
+++ python_bindings/pbi_translation/Translation.pyi
@@ -1,8 +1,13 @@
+class CSharpDict:
+    Keys: list[str]
+    Values: list[str]
+
 class DataViewQueryTranslationResult:
     DaxExpression: str
-    SelectNameToDaxColumnName: dict[str, str]
+    SelectNameToDaxColumnName: CSharpDict
 
 class PrototypeQuery:
     @staticmethod
-    def Translate(query: str, dbName: str, port: int, workingDirectory: str | None = None) -> DataViewQueryTranslationResult: 
-        ...
+    def Translate(
+        query: str, dbName: str, port: int, workingDirectory: str | None = None
+    ) -> DataViewQueryTranslationResult: ...
