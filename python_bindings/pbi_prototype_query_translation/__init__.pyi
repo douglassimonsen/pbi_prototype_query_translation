dd0c0cd25eb81db0262c8661fa90ecf8a8e9fb5d
Matthew Hamilton <mwhamilton6@gmail.com>
2025-08-08 11:59:12 -0500

    Updated stub to fix autocompletion

--- python_bindings/pbi_prototype_query_translation/__init__.pyi
+++ python_bindings/pbi_prototype_query_translation/__init__.pyi
@@ -2,11 +2,12 @@ class CSharpDict:
     Keys: list[str]
     Values: list[str]
 
-
 class DataViewQueryTranslationResult:
     DaxExpression: str
     SelectNameToDaxColumnName: CSharpDict
 
+class TranslationResult:
+    dax: str
+    column_mapping: dict[str, str]
 
-def prototype_query(query: str, db_name: str, port: int) -> "DataViewQueryTranslationResult":
-    ...
+def prototype_query(query: str, db_name: str, port: int) -> "TranslationResult": ...
